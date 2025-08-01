5497622b93114501140f5636637c41de8a8703e0
Beny Adi Nugraha <benyadin17@gmail.com>
2025-08-02 00:47:30 +0700

    fix

--- src/components/ComponentA.tsx
+++ src/components/ComponentA.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../features/productSlice";
--- src/components/ComponentB.tsx
+++ src/components/ComponentB.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../store";
 
